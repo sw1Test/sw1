5bffe7761647e425da411d706ad61eab4e044681
xkovacj1 <xkovacj1@u2-15.cpu.elf.stuba.sk>
2015-10-28 13:41:32 +0100

    dalsia trieda

---
+++ src/swi_pokus1/NewClass2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swi_pokus1;
+
+/**
+ *
+ * @author xkovacj1
+ */
+public class NewClass2 {
+    
+}
